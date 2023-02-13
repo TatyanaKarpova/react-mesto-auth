9edcb3e30b324cb91435ee5c85f00340fd7c669a
Tatyana Karpova <tatyana.pushkareva.1997@mail.ru>
2023-02-13 22:45:51 +0200

    fix: fully provide correct menu opening

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,79 +12,45 @@ function Header ({login, link, onClick, loggedIn, headerText}) {
 
     return (
         <header className='header section'>
-            <div className={`${loggedIn ? 'header__container header__container_mobile' : 'header__container'}`}>
-                <div className={`${loggedIn ? 'logo-container' : ''}`}>
+            {loggedIn ? (
+                <div className='header__container header__container_mobile'>
+                <div className='logo-container'>
                     <img className='header__logo' src={headerLogo} alt='Лого'/>
-                    <button type='button' className={`${!loggedIn ? 'header__menu-button_invisible' : 'header__menu-button'} ${isMenuClicked ? 'header__menu-button_invisible' : 'header__menu-button'}`} onClick={handleMenuClick}></button>
-                    <button type='button' className={`${loggedIn ? 'header__close-button' : 'header__close-button_invisible'} ${isMenuClicked ? 'header__close-button' : 'header__close-button_invisible'}`} onClick={handleMenuClick}></button>
+                    <button type='button' className={`header__menu-button_desktop ${isMenuClicked ? 'header__menu-button_invisible' : 'header__menu-button'}`} onClick={handleMenuClick}></button>
+                    <button type='button' className={`${isMenuClicked ? 'header__close-button' : 'header__close-button_invisible'}`} onClick={handleMenuClick}></button>
                 </div>
-                <div className={`${loggedIn ? 'header__container header__wrapper' : 'header__container'}`}>
-                    <p className={`${isMenuClicked ? 'header__login' : 'header__login_invisable'}`}>{login}</p>
+                <div className={`header__wrapper ${isMenuClicked ? 'header__wrapper_mobile' : ''}`}>
+                    <p className='header__login'>{login}</p>
                     <Link 
                         to={link}
-                        className={`${loggedIn ? 'header__link_invisable' : 'header__link'}`}
+                        className={`${isMenuClicked ? 'header__link_mobile' : 'header__link'}`}
                         onClick={onClick}
                     >
                     {headerText}
                     </Link>
                 </div>
             </div>
-        </header>
-    )
-};
-
-export default Header;
-
-/*
-<div className={`${loggedIn ? 'logo-container' : ''}`}>
-    <img className='header__logo' src={headerLogo} alt='Лого'/>
-    <button type='button' className='header__menu-button' onClick={handleMenuClick}></button>
-</div>
-*/
-
-
-/*
-<div className={`${loggedIn ? 'logo-container' : ''}`}>
-    <img className='header__logo' src={headerLogo} alt='Лого'/>
-    <button type='button' className='header__close-button'></button>
-</div>
-<div className={`${loggedIn ? 'header__container header__wrapper' : 'header__container'}`}>
-    <p className='header__login'>{login}</p>
-    <Link 
-        to={link}
-        className={`${loggedIn ? 'header__link header__link_mobile' : 'header__link'}`}
-        onClick={onClick}
-    >
-        {headerText}
-    </Link>
-</div>
-*/
-
-
-/*
-
-<Link 
-                        to={link}
-                        className={`${loggedIn ? 'header__link_invisable' : 'header__link'} ${isMenuClicked ? 'header__link header__link_mobile' : 'header__link_invisable'}`}
-                        onClick={onClick}
-                    >
-                    {headerText}
-                    </Link>
-
-*/
-
-
-/*
-
-<div className={`${loggedIn ? 'header__container header__wrapper' : 'header__container'} ${isMenuClicked ? 'header__container header__wrapper' : 'header__container'}`}>
-                    <p className={`${isMenuClicked ? 'header__login' : 'header__login_invisable'}`}>{login}</p>
+            ) : (
+                <div className='header__container'>
+                <div className=''>
+                    <img className='header__logo' src={headerLogo} alt='Лого'/>
+                    <button type='button' className='header__menu-button_invisible' onClick={handleMenuClick}></button>
+                    <button type='button' className='header__close-button_invisible' onClick={handleMenuClick}></button>
+                </div>
+                <div className='header__container'>
+                    <p className='header__login'>{login}</p>
                     <Link 
                         to={link}
-                        className={`${loggedIn ? 'header__link_invisable' : 'header__link'}`}
+                        className='header__link'
                         onClick={onClick}
                     >
                     {headerText}
                     </Link>
                 </div>
+            </div>
+            )}            
+        </header>
+    )
+};
 
-*/
+export default Header;
